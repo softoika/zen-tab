63306ced6c33bc926b13a1e20214e3a8810f4e16
softoika <zwgntz@gmail.com>
2020-10-18 09:31:56 +0900

    Setup config files for Chrome extension

---
+++ src/background.ts
@@ -0,0 +1,2 @@
+console.log("Hello, background script!");
+export const foo = "bar";
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: {
+    popup: "./src/popup/Popup.tsx",
+    background: "./src/background.ts"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/")
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "static", to: "." }
+      ]
+    })
+  ],
+  devtool: "inline-source-map",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+};
