93a3a40e154cfad4df436f0ff07f911858cfddaa
softoika <zwgntz@gmail.com>
2020-11-03 09:46:58 +0900

    Add tests of TabStorageService.remove()

--- src/tab-storage-service.test.ts
+++ src/tab-storage-service.test.ts
@@ -35,20 +35,74 @@ describe("TabStorageService", () => {
     service = new TabStorageService(localStorage);
   });
 
-  test("add a tab to empty tabs list", async (done) => {
-    localStorage.get.mockResolvedValue({});
-    const tab = { ...DEFAULT_TAB };
-    await service.add(tab);
-    expect(localStorage.set).toBeCalledWith({ tabs: [tab] });
-    done();
+  afterEach(() => {
+    localStorage.get.mockReset();
+    localStorage.set.mockReset();
   });
 
-  test("add a second tab to tabs list", async (done) => {
-    const tab1 = { ...DEFAULT_TAB };
-    const tab2 = { ...DEFAULT_TAB, id: 2 };
-    localStorage.get.mockResolvedValue({ tabs: [tab1] });
-    await service.add(tab2);
-    expect(localStorage.set).toBeCalledWith({ tabs: [tab1, tab2] });
-    done();
+  describe(".add(tab)", () => {
+    test("add a tab to empty tabs list", async (done) => {
+      localStorage.get.mockResolvedValue({});
+      const tab = { ...DEFAULT_TAB };
+      await service.add(tab);
+      expect(localStorage.set).toBeCalledWith({ tabs: [tab] });
+      done();
+    });
+
+    test("add a second tab to tabs list", async (done) => {
+      const tab1 = { ...DEFAULT_TAB };
+      const tab2 = { ...DEFAULT_TAB, id: 2 };
+      localStorage.get.mockResolvedValue({ tabs: [tab1] });
+      await service.add(tab2);
+      expect(localStorage.set).toBeCalledWith({ tabs: [tab1, tab2] });
+      done();
+    });
+  });
+
+  describe(".remove(tabId)", () => {
+    test("remove the tab from tabs list", async (done) => {
+      const tab = { ...DEFAULT_TAB };
+      localStorage.get.mockResolvedValue({ tabs: [tab] });
+      await service.remove(tab.id);
+      expect(localStorage.set.mock.calls[0][0]?.tabs?.length).toBe(0);
+      done();
+    });
+
+    test("add the tab to history", async (done) => {
+      const tab = { ...DEFAULT_TAB, title: "foo", url: "https://foo.com" };
+      localStorage.get.mockResolvedValue({ tabs: [tab] });
+      await service.remove(tab.id);
+      expect(localStorage.set.mock.calls[0][0]?.history?.[0]).toEqual({
+        title: "foo",
+        url: "https://foo.com",
+        favIconUrl: undefined,
+      });
+      done();
+    });
+
+    test("does'nt remove anything if the matching tabId isn't exist", async (done) => {
+      const tab = { ...DEFAULT_TAB };
+      localStorage.get.mockResolvedValue({ tabs: [tab] });
+      await service.remove(undefined);
+      await service.remove(9999);
+      expect(localStorage.set).toBeCalledTimes(0);
+      done();
+    });
+
+    test("use pendingUrl in history if url is falsy", async (done) => {
+      const tab = {
+        ...DEFAULT_TAB,
+        title: "Bookmarks",
+        pendingUrl: "chrome://bookmarks",
+      };
+      localStorage.get.mockResolvedValue({ tabs: [tab] });
+      await service.remove(tab.id);
+      expect(localStorage.set.mock.calls[0][0]?.history?.[0]).toEqual({
+        title: "Bookmarks",
+        url: "chrome://bookmarks",
+        favIconUrl: undefined,
+      });
+      done();
+    });
   });
 });
