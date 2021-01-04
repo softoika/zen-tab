d0339f0acb2c229bdcfbc7032fde9c855c2d3b99
softoika <zwgntz@gmail.com>
2021-01-04 15:56:27 +0900

    Improve bundle sizes in production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,45 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const isProd = mode === "production";
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
+  mode,
   entry: {
     popup: "./src/popup/Popup.tsx",
-    background: "./src/background.ts"
+    background: "./src/background.ts",
   },
   output: {
-    path: path.resolve(__dirname, "dist/")
+    path: path.resolve(__dirname, "dist/"),
   },
   module: {
     rules: [
-      { 
+      {
         test: /\.tsx?$/,
-        loader: "ts-loader"
-      }
-    ]
+        loader: "ts-loader",
+      },
+    ],
   },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
         { from: "static", to: "." },
-        { from: "node_modules/webextension-polyfill/dist/browser-polyfill.js" }
-      ]
-    })
+        { from: "node_modules/webextension-polyfill/dist/browser-polyfill.js" },
+      ],
+    }),
   ],
-  devtool: "inline-source-map",
+  devtool: !isProd && "inline-source-map",
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  }
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  optimization: {
+    minimize: isProd,
+    minimizer: [new TerserWebpackPlugin({ 
+      terserOptions: {
+        compress: { drop_console: true }
+      }
+    })],
+  },
 };
