2f8bb99092203df0a1ddefff0ee11e7f1161c7d6
softoika <zwgntz@gmail.com>
2021-03-20 09:44:32 +0900

    Fix to hide time left of an active tab

--- src/popup/TabsStatus.tsx
+++ src/popup/TabsStatus.tsx
@@ -46,12 +46,10 @@ export const TabsStatus: React.FC<{ page: Page }> = ({ page }) => {
         <li key={tab.id}>
           <div>
             {timeLeftMap?.[tab.id ?? 0] && (
-              <>
-                <span>{timeLeftMap?.[tab.id ?? 0]?.minus && "-"}</span>
-                <span>{timeLeftMap?.[tab.id ?? 0]?.hours}</span>
-                <span>:</span>
-                <span>{timeLeftMap?.[tab.id ?? 0]?.mins}</span>
-              </>
+              <TimeLeftOfTab
+                timeLeft={timeLeftMap?.[tab.id ?? 0]}
+                active={tab.active}
+              />
             )}
           </div>
           <img
@@ -67,6 +65,23 @@ export const TabsStatus: React.FC<{ page: Page }> = ({ page }) => {
         </li>
       ))}
     </ul>
+  );
+};
+
+const TimeLeftOfTab: React.FC<{ timeLeft?: TimeLeft; active?: boolean }> = ({
+  timeLeft,
+  active,
+}) => {
+  if (timeLeft == null || active) {
+    return null;
+  }
+  return (
+    <>
+      <span>{timeLeft.minus && "-"}</span>
+      <span>{timeLeft.hours}</span>
+      <span>:</span>
+      <span>{timeLeft.mins}</span>
+    </>
   );
 };
 
