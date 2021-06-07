0175c841bd1791bd68b7b753fa8c7fbed94b30db
softoika <zwgntz@gmail.com>
2021-06-07 20:20:47 +0900

    Show active or pinned tab status in the popup

--- src/popup/TabsStatus/TabsStatus.tsx
+++ src/popup/TabsStatus/TabsStatus.tsx
@@ -1,5 +1,6 @@
 import { FavIcon } from "popup/components/FavIcon";
 import React from "react";
+import type { Tab } from "types";
 import type { Page } from "../types";
 import { useTabs } from "./useTabs";
 import type { TimeLeft } from "./useTimeLeftMap";
@@ -14,11 +15,8 @@ export const TabsStatus: React.FC<{ page: Page }> = ({ page }) => {
   return (
     <ul className="space-y-4">
       {tabs.map((tab) => (
-        <li key={tab.id} className="flex space-x-3 items-start">
-          <TimeLeftOfTab
-            timeLeft={timeLeftMap?.[tab.id ?? 0]}
-            active={tab.active}
-          />
+        <li key={tab.id} className="flex space-x-3 items-center">
+          <TimeLeftOfTab timeLeft={timeLeftMap?.[tab.id ?? 0]} tab={tab} />
           <FavIcon
             favIconUrl={tab.favIconUrl}
             url={tab.url ?? tab.pendingUrl}
@@ -30,13 +28,38 @@ export const TabsStatus: React.FC<{ page: Page }> = ({ page }) => {
   );
 };
 
-const TimeLeftOfTab: React.FC<{ timeLeft?: TimeLeft; active?: boolean }> = ({
+const TimeLeftOfTab: React.FC<{ timeLeft?: TimeLeft; tab: Tab }> = ({
   timeLeft,
-  active,
+  tab,
 }) => {
   const timeLeftText =
-    timeLeft != null && !active
+    timeLeft != null
       ? `${timeLeft.minus ? "-" : ""}${timeLeft.hours}:${timeLeft.mins}`
       : "";
-  return <div className="flex justify-end min-w-[35px]">{timeLeftText}</div>;
+  return (
+    <div className="flex-grow-0 flex-shrink-0 w-[50px]">
+      <TimeLeftContent timeLeftText={timeLeftText} tab={tab} />
+    </div>
+  );
+};
+
+const TimeLeftContent: React.VFC<{ timeLeftText: string; tab: Tab }> = ({
+  timeLeftText,
+  tab,
+}) => {
+  if (tab.active) {
+    return (
+      <div className="flex justify-center p-1 bg-green-300 rounded-sm transform scale-75">
+        ACTIVE
+      </div>
+    );
+  } else if (tab.pinned) {
+    return (
+      <div className="flex justify-center p-1 bg-gray-300 rounded-sm transform scale-75">
+        PINNED
+      </div>
+    );
+  } else {
+    return <div className="text-right">{timeLeftText}</div>;
+  }
 };
